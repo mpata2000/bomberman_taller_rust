9704a1c42d1aecdf1a217ce4b6ccf8720259b383
Martin Pata <martin@DESKTOP-50TNM2U>
2023-09-17 18:44:13 -0300

    file structure

---
+++ src/enemy.rs
@@ -0,0 +1,5 @@
+
+struct Enemy {
+    health: u32,
+    position: Point,
+}
--- src/main.rs
+++ src/main.rs
@@ -14,36 +14,32 @@ fn validate_args(args: Vec<String>) -> Result<(String, String, Point), String> {
     }
     let input_path = args[0].clone();
     let output_path = args[1].clone();
-    let temp_x = args[2].parse::<u32>();
-    let temp_y = args[3].parse::<u32>();
-    match (temp_x, temp_y) {
-        (Ok(x), Ok(y)) => {
-            if x < 0 || y < 0 {
-                return Err(format!("x and y must be greater than 0"));
-            }
+    let x = args[2].parse::<u32>();
+    let y = args[3].parse::<u32>();
+
+    match (x, y) {
+        (Ok(x), Ok(y)) if x > 0 && y > 0 => {
+            Ok((
+                input_path,
+                output_path,
+                Point { x, y },
+            ))
         }
-        (Err(_), Ok(_)) => return Err(format!("x must be a number")),
-        (Ok(_), Err(_)) => return Err(format!("y must be a number")),
-        (Err(_), Err(_)) => return Err(format!("x and y must be numbers")),
+        (Ok(_), Ok(_)) => Err("x and y must be greater than 0".to_string()),
+        (Err(_), Ok(_)) => Err("x must be a number".to_string()),
+        (Ok(_), Err(_)) => Err("y must be a number".to_string()),
+        (Err(_), Err(_)) => Err("x and y must be numbers".to_string()),
     }
-    Ok((
-        input_path,
-        output_path,
-        Point {
-            x: temp_x.unwrap_or(0),
-            y: temp_y.unwrap_or(0),
-        },
-    ))
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
-    if args.len() != 4 {
-        println!(
-            "Incorrect number of arguments provided, need 4 got {}",
-            args.len()
-        );
-        println!("Usage: cargo run -- <input.txt> <output_path> <x> <y>");
-        return;
-    }
+    let (input_path, output_path, start_point) = match validate_args(args) {
+        Ok((input_path, output_path, point)) => (input_path, output_path, point),
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
 }
