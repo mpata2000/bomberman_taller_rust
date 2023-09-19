7ef5f6807cb79f940f5e22f68b83433299421060
Martin Pata <mpata2000@gmail.com>
2023-09-19 20:57:59 -0300

    Add integration test

--- src/bomberman.rs
+++ src/bomberman.rs
@@ -50,7 +50,7 @@ impl Bomberman {
     // Create a new game from a string
     // The string should be a square matrix of squares separated by spaces
     pub(crate) fn new(file_string: String) -> Result<Bomberman, BombermanError> {
-        let lines: Vec<&str> = file_string.split('\n').collect();
+        let lines: Vec<&str> = file_string.trim().split('\n').collect();
 
         let mut game = Bomberman {
             enemies: Vec::new(),
@@ -60,7 +60,7 @@ impl Bomberman {
         };
 
         for (y, line) in lines.iter().enumerate() {
-            let squares: Vec<&str> = line.split(' ').collect();
+            let squares: Vec<&str> = line.trim().split(' ').collect();
             if squares.len() != game.size as usize {
                 return Err(BombermanError::MazeNotSquare(format!(
                     "Maze has {} lines and {} columns, it should be equal",
@@ -204,5 +204,38 @@ impl Display for Bomberman {
             display.push('\n');
         }
         write!(f, "{}", display)
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    // Next 3 integration test come from https://taller-1-fiuba-rust.github.io/proyecto/23C2/ejercicio_individual.html
+    #[test]
+    fn integration_test_example_1() {
+        let input = "B2 R R _ F1 _ _\n_ W R W _ W _\nB5 _ _ _ B2 _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n".to_string();
+        let result = "_ R R _ _ _ _\n_ W R W _ W _\n_ _ _ _ _ _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n".to_string();
+        let mut game = Bomberman::new(input).unwrap();
+        let board = game.play(Point::new(0, 0)).unwrap();
+        assert_eq!(result, board);
+    }
+
+    #[test]
+    fn integration_test_example_2(){
+        let input = "_ _ B2 _ B1 _ _\n_ W _ W _ W _\n_ _ B2 R F1 _ _\n_ W _ W R W _\n_ _ B4 _ _ _ _\n_ W _ W _ W _\n_ _ _ _ _ _ B1\n".to_string();
+        let result = "_ _ _ _ _ _ _\n_ W _ W _ W _\n_ _ _ R F1 _ _\n_ W _ W R W _\n_ _ _ _ _ _ _\n_ W _ W _ W _\n_ _ _ _ _ _ B1\n".to_string();
+        let mut game = Bomberman::new(input).unwrap();
+        let board = game.play(Point::new(2, 4)).unwrap();
+        assert_eq!(result, board);
+    }
+
+    #[test]
+    fn integration_test_example_3(){
+        let input = "_ _ _ _ _ _ _\n_ W _ W _ W _\nS4 R R R F2 _ _\n_ W _ W _ W _\nB2 _ B5 _ DU _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n".to_string();
+        let result = "_ _ _ _ _ _ _\n_ W _ W _ W _\n_ R R R _ _ _\n_ W _ W _ W _\n_ _ _ _ DU _ _\n_ W _ W _ W _\n_ _ _ _ _ _ _\n".to_string();
+        let mut game = Bomberman::new(input).unwrap();
+        let board = game.play(Point::new(2, 4)).unwrap();
+        assert_eq!(result, board);
     }
 }
